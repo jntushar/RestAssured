accfd83891c4765661c61e7b8b9456d39f018742
jntushar <tjain4736@gmail.com>
2020-05-23 22:15:08 +0530

    Data from excel file

---
+++ RestAssuredPratice/src/test/java/utils/ExcelsUtilsTest.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class ExcelsUtilsTest {
+
+	public static void main(String[] args) {
+		
+		String excelPath = "./Data/TestData.xlsx";
+		String sheetName = "Sheet1";
+		ExcelUtils excel = new ExcelUtils(excelPath, sheetName);
+		
+		excel.getRowCount();
+		excel.getCellData(1, 0);
+		excel.getCellData(1, 1);
+		excel.getCellData(1, 2);
+	}
+}
